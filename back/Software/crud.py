84b732be1f41d193f4e07210776c3b5660184a3c
mishelevine <mishelevine@gmail.com>
2025-01-14 01:03:03 +0500

    Update+delete software

--- back/Software/crud.py
+++ back/Software/crud.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from back.Software.models import Software
@@ -22,7 +23,7 @@ async def create_software(software: SSoftwareCreate):
         await session.refresh(db_software)
         return db_software
     
-async def get_software_by_id(software_id: int) -> SSoftware:
+async def get_software_by_id(software_id: int):
     async with async_session() as session:
         query = select(Software).filter(Software.id == software_id)
         result = await session.execute(query)
@@ -33,3 +34,23 @@ async def get_all_software() -> list[Software]:
         query = select(Software)
         result = await session.execute(query)
         return result.scalars().all()
+    
+async def delete_software(software_id: int):
+    async with async_session() as session:
+        job = await get_software_by_id(software_id)
+        
+        if not job:
+            raise HTTPException(status_code=404, detail="Software not found")
+        
+        await session.delete(job)
+        await session.commit()
+        return {"detail": "Software deleted successfully"}
+    
+async def update_software(existing_software: Software):
+    async with async_session() as session:
+            session.add(existing_software)
+            session.merge(existing_software)
+            await session.commit()
+            await session.refresh(existing_software)
+    
+    return existing_software
